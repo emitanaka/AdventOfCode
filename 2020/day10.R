4be704ed80e20f3178cf5b2b4cfc0f6e837e4e3f
Emi Tanaka <dr.emi.tanaka@gmail.com>
2020-12-20 18:10:31 +1100

    day 10 part 2 explanation added

--- 2020/day10.R
+++ 2020/day10.R
@@ -10,13 +10,20 @@ x2 <- c(0, x, max(x) + 3)
 prod(table(diff(x2)))
 
 # part 2 ------------------------------------------------------------------
+# if only 1 diff=1 in a row, then one way
+sum(choose(1, 0:1)) # if 2 diff=1 in a row, then one must be picked,
+                    # the other can be removed or not
+sum(choose(2, 0:2)) # for 3, then 0, 1 or 2 can be removed
+sum(choose(3, 0:2)) # for 4, then 0, 1 or 2 can be removed.
+                    # 3 cannot be removed, because one needed for diff <= 3.
+combs <- c(1, 2, 4, 7)
 
 options("scipen" = 14)
-rle(diff(x2)) %>%
+rle(diff(x2)) %>% # diff values are 1 or 3
   unclass() %>%
-  as.data.frame() %>%
+  as.data.frame() %>% # when diff is 3, there is no choice
   filter(values == 1) %>%
-  mutate(comb = map_dbl(lengths, ~sum(choose(.x - 1, 0:2)))) %>%
+  mutate(comb = combs[lengths]) %>%
   pull(comb) %>%
   prod()
 
